31eab0bae7fea2eb0673217cf935594d8508f2d4
Ashutosh Sahu <110385580+ashuRMS@users.noreply.github.com>
2023-08-16 19:00:57 +0530

    Update p1_B.py

--- Solutions/p1_B.py
+++ Solutions/p1_B.py
@@ -2,49 +2,81 @@
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
-from math import pow,sqrt,atan2
-import roslaunch
+from math import pow,sqrt
+import transformations as tf
+import math
 
 class go_to_goal:
     def __init__(self):
-        rospy.init_node('turtle_controller',anonymous=True)   # Intialize the the turtle_controller node
+        # Intialize the the turtle_controller node
+        rospy.init_node('turtle_controller',anonymous=True)   
 
-        pub_topic="/turtle1/cmd_vel"                          # Publisher topic
-        sub_topic="/turtle1/pose"                             # Subscriber topic
+        # Create Publisher and Subscriber object
+        pub_topic="/turtle1/cmd_vel"                          
+        sub_topic="/turtle1/pose"                            
 
         self.pub = rospy.Publisher(pub_topic, Twist, queue_size=10)
         self.sub = rospy.Subscriber(sub_topic, Pose, self.update_pose)
 
-        self.pose=Pose()                                       # pose object
-        self.rate =rospy.Rate(20)
+        # Create pose object
+        self.pose=Pose()                                       
+        self.rate =rospy.Rate(50)
     
     def update_pose(self,data):
         self.pose=data
         # The data parameter is already being passed in the subscriber object. This is the pecuilarity of ROS framework
-        self.pose.x=round(self.pose.x,4)
-        self.pose.y=round(self.pose.y,4)
+        # Update the pose
+        self.pose.x=self.pose.x
+        self.pose.y=self.pose.y
     
     def euclidean_distance(self,goal_pose):
+        # Calculate the euclidean distance between desired and current position
         return sqrt(pow((goal_pose.x - self.pose.x),2) + pow((goal_pose.y - self.pose.y),2))
     
     def linear_vel(self,goal_pose,kp):
+        # Calculate linear velocity based on euclidean distance. Kp is gain parameter
         return kp*(self.euclidean_distance(goal_pose))
     
     def heading_dirn(self,goal_pose):
-        return atan2((goal_pose.y - self.pose.y),(goal_pose.x - self.pose.x))
+        # Calculate the heading diretion
+        delta_y = goal_pose.y - self.pose.y
+        delta_x = goal_pose.x - self.pose.x
+        return math.atan2(delta_y, delta_x)
+    
+    def calculate_angle_difference(self,angle2, angle1): 
+        # Create quaternion from angle1 and angle2
+        q1 = tf.transformations.quaternion_from_euler(0, 0,angle1)
+        q2 = tf.transformations.quaternion_from_euler(0, 0,angle2)
+        
+        # Calculate quaternion difference
+        q_diff = tf.transformations.quaternion_multiply(q2, tf.transformations.quaternion_inverse(q1))
+        
+        # Convert quaternion difference back to angles
+        roll, pitch, yaw = tf.transformations.euler_from_quaternion(q_diff)
+        angle_diff = yaw
+        return angle_diff
     
     def angular_vel(self,goal_pose,kw):
-        return kw*(self.heading_dirn(goal_pose) - self.pose.theta)
-    
+        # Calculate the angular velocities
+        return kw*self.calculate_angle_difference(self.heading_dirn(goal_pose),self.pose.theta)
+
     def going_to_goal(self,x,y):
         goal_pose=Pose()
 
         goal_pose.x= x #1.42
-        # For different gain values we will get some coordinates that causes mad behaviour
         goal_pose.y= y  #9
+        
+        # Tolerance
         error=0.001
 
-        vel_msg=Twist()                                         # Twist Object
+        # Initial position
+        self.pose.x=5.5444
+        self.pose.y=5.5444
+
+        # Create Twist Object
+        vel_msg=Twist()   
+
+        # Move until the end                                      
         while self.euclidean_distance(goal_pose)>=error:
             vel_msg.linear.x = self.linear_vel(goal_pose,1.5)
             vel_msg.linear.y = 0
@@ -52,8 +84,7 @@ class go_to_goal:
 
             vel_msg.angular.x=0
             vel_msg.angular.y=0
-            vel_msg.angular.z=self.angular_vel(goal_pose,6)
-            
+            vel_msg.angular.z=self.angular_vel(goal_pose,10)
             self.pub.publish(vel_msg)
             self.rate.sleep()
 
@@ -82,23 +113,22 @@ class go_to_goal:
 
 if __name__=="__main__":
     try:
-        node=roslaunch.core.Node("turtlesim","turtlesim_node")
-        launch=roslaunch.scriptapi.ROSLaunch()
-        launch.start()
-        process=launch.launch(node)
+
+        x=float(input("Enter the x-coordinate: "))
+        y=float(input("Enter the y-coordinate: "))
         turtle=go_to_goal()
         print("************************************************************")
-        turtle.going_to_goal(9.45,5.45)
+        turtle.going_to_goal(x,y)
         print("Reached first goal. \n", flush=True)
         print("************************************************************")
-        turtle.going_to_goal(9.45,9.45)
-        print("Reached second goal. \n", flush=True)
-        print("************************************************************")
-        turtle.going_to_goal(5.45,5.45)
-        print("Reached third goal. \n", flush=True)
-        print("************************************************************")
-        turtle.orient_myway(1.57)
-        print("Oriented as per my way \n",flush=True)
+        # turtle.going_to_goal(y,x)
+        # print("Reached second goal. \n", flush=True)
+        # print("************************************************************")
+        # turtle.going_to_goal(5.45,5.45)
+        # print("Reached third goal. \n", flush=True)
+        # print("************************************************************")
+        # turtle.orient_myway(1.57)
+        # print("Oriented as per my way \n",flush=True)
     except rospy.ROSInterruptException:
         pass       
         
